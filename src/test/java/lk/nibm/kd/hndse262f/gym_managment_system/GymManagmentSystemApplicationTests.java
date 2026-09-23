62a3fdf3dda4209d89c368078a15fe246fd00361
nvpkarunaratne1127 <nvpkarunaratne@gmail.com>
2026-09-23 22:38:31 +0530

    init commit

---
+++ src/test/java/lk/nibm/kd/hndse262f/gym_managment_system/GymManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.nibm.kd.hndse262f.gym_managment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
